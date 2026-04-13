a8ee3e6892afe467e3a30cf388d805d3ef169f3b
DasBoot1510 <p.schiff89@gmail.com>
2026-04-13 09:03:51 +0200

    Added groups for updates and drawing

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     gameClock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+
         dt = gameClock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         
 
