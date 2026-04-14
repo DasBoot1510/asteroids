b766f51a3c60f6e416414775b00505cb38bcd8ec
DasBoot1510 <p.schiff89@gmail.com>
2026-04-14 19:16:55 +0200

    Implemented Asteroids

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pygame
+import pygame 
 from player import Player
+from asteroid import Asteroid
 from logger import log_state
 from constants import *
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,9 +13,17 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidField = AsteroidField()
 
     gameClock = pygame.time.Clock()
     dt = 0
